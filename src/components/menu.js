9b8c73e218b0c8e3f760efb367f05d8dadeffa87
kikkuru9 <147474849+kikkuru9@users.noreply.github.com>
2024-05-14 17:07:26 +0530

    updated

--- src/components/menu.js
+++ src/components/menu.js
@@ -9,7 +9,7 @@ function MenuComponent(){
             <nav class="navbar navbar-expand-lg bg-body-success" id="menubar">
   <div class="container-fluid">
     <div class='col-6'>
-    <a class="navbar-brand" href="#fotter"><img src='aflogo.jpeg' class='logo'/></a></div>
+    <a class="navbar-brand" href="#fotter"><img src='aflogo.jpeg' class='logo' id="logos"/></a></div>
     <div class='clo-6'><button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button></div>
